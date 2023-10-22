a3a45818860a0e849043f76982032d27364d4b75
Beyler55 <baylarss@code.edu.az>
2023-10-22 22:28:58 +0400

    Work Queues

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://auoikmme:f5lpInfFbr3y9XJgyKVIEoNOZXhQPeDQ@fish.rmq.cloudamqp.com/auoikmme");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
+
+channel.BasicQos(0, 1, false);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://auoikmme:f5lpInfFbr3y9XJgyKVIEoNOZXhQPeDQ@fish.rmq.cloudamqp.com/auoikmme");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
+
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+byte[] message = Encoding.UTF8.GetBytes("Hello World!");
+channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties: properties);
+
+Console.Read();
