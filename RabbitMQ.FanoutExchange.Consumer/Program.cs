137d38e656cc14b0ec8ff991a2d8cb4b8e7bbccc
Beyler55 <baylarss@code.edu.az>
2023-10-23 15:15:21 +0400

    Fanout Exchange

---
+++ RabbitMQ.FanoutExchange.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://auoikmme:f5lpInfFbr3y9XJgyKVIEoNOZXhQPeDQ@fish.rmq.cloudamqp.com/auoikmme");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-example",
+    type: ExchangeType.Fanout);
+
+string queueName = Console.ReadLine();
+channel.QueueDeclare(queueName, exclusive: false);
+channel.QueueBind(queueName,"fanout-exchange-example",routingKey: string.Empty);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queueName,autoAck: true, consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
