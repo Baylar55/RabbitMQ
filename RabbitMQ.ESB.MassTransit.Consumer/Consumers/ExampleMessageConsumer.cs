a85d96032ee614c0adf45ff70ec93b85f8c9deb3
Beyler55 <baylarss@code.edu.az>
2023-10-26 12:31:01 +0400

    Enterprise Service Bus/MassTransit in Console Apps

---
+++ RabbitMQ.ESB.MassTransit.Consumer/Consumers/ExampleMessageConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.Shared.Messages;
+
+namespace RabbitMQ.ESB.MassTransit.Consumer.Consumers
+{
+    public class ExampleMessageConsumer: IConsumer<IMessage>
+    {
+        public Task Consume(ConsumeContext<IMessage> context)
+        {
+            Console.WriteLine($"Received Message : {context.Message.Text}");
+            return Task.CompletedTask;
+        }
+    }
+}
